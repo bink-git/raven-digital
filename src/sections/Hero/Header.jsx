38e02bc4e57714dfa5f27edfa9178e0173baff82
BINK <bink@email.ua>
2023-10-06 19:29:53 +0300

    add animation to logo

--- src/sections/Hero/Header.jsx
+++ src/sections/Hero/Header.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import logo from "../../assets/logo.svg";
 import { links } from "../../data.js";
+import "animate.css";
 
 const Header = () => {
   return (
     <div className="container-2xl mx-auto flex w-full items-center justify-between px-[120px] pt-12">
       <div>
         <a href="#">
-          <img src={logo} width={156} height={52} alt="main logo" />
+          <img
+            src={logo}
+            width={156}
+            height={52}
+            alt="main logo"
+            className="animate__animated animate__slow animate__fadeIn"
+          />
         </a>
       </div>
       <nav className="">
